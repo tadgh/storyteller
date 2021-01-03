3251f92cef60f59e87cea6de6c15150d34324153
Daniel Kivi <sugarfreedanny@gmail.com>
2021-01-03 01:51:36 -0500

    Added key checking middleware
    - Also added a .env.example file

--- src/main.py
+++ src/main.py
@@ -1,34 +1,53 @@
 import asyncio
+import os
 
 from sanic import Sanic
-from sanic.response import json
-# from bot import StoryTeller
+from sanic.response import json, empty
+from sanic.request import RequestParameters
+from dotenv import load_dotenv
 from bot import StoryTeller
 
+load_dotenv()
 app = Sanic(name="zoop")
 bot = StoryTeller()
+
+
+@app.middleware('request')
+async def check_key(request):
+    if request.args['token'][0] != os.getenv('SECRET_KEY'):
+        return empty(status=401)
 
 
 @app.route('/')
 async def test(request):
     return json({'hello': 'world'})
 
+
 @app.route('/go_to_sleep')
 async def test(request):
     await bot.go_to_sleep()
     return json({'slept': 'yes'})
+
 
 @app.route('/wake_up')
 async def test(request):
     await bot.wake_up()
     return json({'woken': 'yes'})
 
+
 @app.route('/wake_up_gently')
 async def test(request):
     await bot.wake_up_gently()
     return json({'woken': 'yes'})
 
-webserver = app.create_server(host='0.0.0.0', port=8000, debug=True, return_asyncio_server=True)
+debug = os.getenv('SERVER_DEBUG') == 'True'
+webserver = app.create_server(
+    host=os.getenv('SERVER_HOST'),
+    port=os.getenv('SERVER_PORT'),
+    debug=debug,
+    return_asyncio_server=True
+)
+
 asyncio.ensure_future(webserver)
 
 loop = asyncio.get_event_loop()
